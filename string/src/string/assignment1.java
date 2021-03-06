8c3983a7f92acfa41c903b3ae29c19b98d24f98b
TERESA Hidalgo De Fitzgerald <tfitzge134@gmail.com>
2021-03-06 07:31:22 -0600

    string/

---
+++ string/src/string/assignment1.java
@@ -0,0 +1,16 @@
+package string;
+
+public class assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Write a class named Assignment1 that declares two strings, “Hello” and (your name) and concatenates
+	//	them together. Print the result.
+		String greetings = "Hello";
+		String myName = "Teresa";
+		String allGreetings = greetings +  " " + myName;
+			
+		System.out.println(" all greeting is " + allGreetings); 
+	}
+	  
+}
