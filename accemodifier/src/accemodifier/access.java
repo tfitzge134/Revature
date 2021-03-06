01caac5fbb3f6df9ceb986141472e1514e171f01
TERESA Hidalgo De Fitzgerald <tfitzge134@gmail.com>
2021-03-06 07:06:01 -0600

    accemodifier

---
+++ accemodifier/src/accemodifier/access.java
@@ -0,0 +1,16 @@
+package accemodifier;
+
+public class access {
+
+	public static void main(String[] args) {
+		/*
+		 * public -everywerhere
+		 * protected -within hte pacage and if any class has extendd
+		 * 
+		 */
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ accemodifier/src/accemodifier/modifier.java
@@ -0,0 +1,5 @@
+package accemodifier;
+
+public class modifier {
+
+}
