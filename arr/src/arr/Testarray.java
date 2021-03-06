e49272910927c18e5d3407ee0121485f0fcdc56f
TERESA Hidalgo De Fitzgerald <tfitzge134@gmail.com>
2021-03-06 07:11:36 -0600

    arr

---
+++ arr/src/arr/Testarray.java
@@ -0,0 +1,23 @@
+package arr;
+
+public class Testarray {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//declare arry
+		int a[] = new int[5]; //an arry of int wiht a size of 5
+		a[0] = 10;
+		a[1] = 20;
+		a[2]=70;
+		a[3]=40;
+		a[4] = 50;
+		//looping tough array
+		for (int i=0; i< a.length; i++)
+		System.out.println(a[i]);
+	
+				
+
+	}
+
+}
